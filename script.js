8edc3d1b384f14b850423a5941f72790794a6b18
a3erachezx <dy.lancarrera@hotmail.es>
2025-01-13 11:35:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 document.getElementById('yes-btn').addEventListener('click', function() {
   const message = document.getElementById('response-message');
-  message.innerHTML = "<h2>Yay! 💖 I’m so happy! Let's make this Valentine’s Day unforgettable!</h2>";
+  message.innerHTML = "<h2>Yay! 💖 I’m so happy! Let's make this Valentine’s Day unforgettable 😘🌹, I love you.!</h2>";
   message.classList.remove('hidden');
   document.querySelector('h1').classList.add('hidden');
   document.querySelector('.message').classList.add('hidden');
@@ -11,7 +11,7 @@ document.getElementById('yes-btn').addEventListener('click', function() {
 
 document.getElementById('no-btn').addEventListener('click', function() {
   const message = document.getElementById('response-message');
-  message.innerHTML = "<h2>Oh no! 😢 Maybe next year...</h2>";
+  message.innerHTML = "<h2>Haha Sorry, You can't say no 😚h2>";
   message.classList.remove('hidden');
   document.querySelector('h1').classList.add('hidden');
   document.querySelector('.message').classList.add('hidden');
